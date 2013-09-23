c547f144428667ab8bc549850aa9b039aad7b02e
Alan Ly <me@alanly.ca>
2013-09-23 11:49:31 -0400

    Added configuration store for metadata API services.

---
+++ app/config/metadata.php
@@ -0,0 +1,23 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Metadata API Keys
+    |--------------------------------------------------------------------------
+    | Here are the metadata sources that are setup for the application.
+    | This should contain the appropriate API key assigned to your account
+    | on each specific service.
+    |--------------------------------------------------------------------------
+    */
+    'connections' => array(
+
+        // Trakt.TV API
+        'trakt' => array(
+            'key' => '1edc74874f676ae6c67b22ebdc511f35',
+        ),
+
+    ),
+
+);
