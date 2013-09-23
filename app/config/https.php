404ca1c38d8e72c766f6c4edf54f5252c1c35a8c
Alan Ly <me@alanly.ca>
2013-09-23 12:24:26 -0400

    Added `force secure` configuration variable to allow user modifiable behavior.

---
+++ app/config/https.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Force redirect to secure URL.
+    |--------------------------------------------------------------------------
+    | Enabling this option allows the application to redirect the user to
+    | the SSL enabled ('https') URL of the page for every request.
+    |--------------------------------------------------------------------------
+    */
+    'force_secure' => false,
+
+);
