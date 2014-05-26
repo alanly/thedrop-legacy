d00554ee8a6b5d8fd874d48a55cc503856d3eab1
Stewart Adam <s.adam@diffingo.com>
2014-05-26 12:19:31 -0400

    Add missing file from commit 7706d21

---
+++ app/config/thedrop.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+  /*
+  |--------------------------------------------------------------------------
+  | Media watch path
+  |--------------------------------------------------------------------------
+  |
+  | The full filesystem path to the folder that will be analyzed for media.
+  |
+  */
+  'watch_path' => '/Users/firewing/devel/git/thedrop/repo_files',
+
+);
